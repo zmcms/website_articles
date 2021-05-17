dbd95f9bc07e5ff77cdf5a16d5f2913a54c8e257
zmcms <59611848+zmcms@users.noreply.github.com>
2021-05-17 15:41:02 +0200

    Add files via upload

---
+++ src/backend/views/zmcms_articles_list.blade.php
@@ -0,0 +1,10 @@
+<ul class="navigations_tree">
+	@foreach($resultset as $r)
+	<li>
+		<a id="article_edit_{{$r->token}}" data-token="{{ $r->token }}" data-objslug="{{$r->slug}}"  href="#"><span class="far fa-edit"></span></a>
+   		<a id="article_link_{{$r->token}}" data-token="{{ $r->token }}" data-objslug="{{$r->slug}}"  data-type="articles" href="#"><span class="fas fa-link"></span></a>
+   		<a id="article_del_{{$r->token}}" data-token="{{ $r->token }}"  data-objslug="{{$r->slug}}" href="#"><span class="fas fa-trash-alt"></span></a>
+		<a href="#" target="_blank">{{$r->title}}</a>
+	</li>
+	@endforeach
+</ul>
